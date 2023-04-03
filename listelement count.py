0aa90ce9bc8a97c2557965de5306380eb279f0a3
kalakotijayanth <jayanthkalakoti@gmail.com>
2023-04-03 11:22:23 +0530

    jj

---
+++ listelement count.py
@@ -0,0 +1,11 @@
+# list1=list(map(int,input().split()))
+# ele=int(input())
+# count=0
+# for i in list1:
+#     if i==ele:
+#         count=count+1
+#
+# print(count)
+list1=list(map(int,input().split()))
+list2= (list1)
+print(list1)
